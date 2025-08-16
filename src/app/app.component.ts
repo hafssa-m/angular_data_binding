512ceb9d19a813c47cc4c0948fb07f0b7acc3582
hafssa-m <hafsaa8989@gmail.com>
2025-08-17 02:38:19 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'daynine';
+  name = 'hafssa';
+  email = 'hafssa@33.com';
+
+  showGreeting = false;
+
+  updateName(newName: string) {
+    this.name = newName;
+  }
 }
